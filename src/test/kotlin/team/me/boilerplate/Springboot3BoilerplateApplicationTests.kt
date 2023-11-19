0732d8d90b35b09880122b0f16a2ee08d2e35b3a
BrianDYKim <18k7102dy@gmail.com>
2023-11-19 22:20:42 +0900

    project init

---
+++ src/test/kotlin/team/me/boilerplate/Springboot3BoilerplateApplicationTests.kt
@@ -0,0 +1,13 @@
+package team.me.boilerplate
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Springboot3BoilerplateApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
