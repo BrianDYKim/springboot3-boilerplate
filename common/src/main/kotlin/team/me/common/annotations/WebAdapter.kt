bfc5b8b0238f32a29fa8ac13de59a1aa12fc1d9c
BrianDYKim <18k7102dy@naver.com>
2024-01-19 01:00:55 +0900

    common에 selfValidator, 어노테이션들 추가

---
+++ common/src/main/kotlin/team/me/common/annotations/WebAdapter.kt
@@ -0,0 +1,13 @@
+package team.me.common.annotations
+
+import org.springframework.core.annotation.AliasFor
+import org.springframework.stereotype.Component
+
+@Target(AnnotationTarget.CLASS, AnnotationTarget.TYPE)
+@Retention(AnnotationRetention.RUNTIME)
+@MustBeDocumented
+@Component
+annotation class WebAdapter(
+    @get:AliasFor(annotation = Component::class)
+    val value: String = ""
+)
---
+++ common/src/main/kotlin/team/me/common/validate/SelfValidating.kt
@@ -0,0 +1,20 @@
+package team.me.common.validate
+
+import jakarta.validation.ConstraintViolationException
+import jakarta.validation.Validation
+
+/**
+ * @author Brian
+ * @since 1/19/24
+ */
+open class SelfValidating<T: Any> {
+    private val validator = Validation.buildDefaultValidatorFactory().validator
+
+    protected fun validateSelf() {
+        val violations = validator.validate(this)
+
+        if (violations.isNotEmpty()) {
+            throw ConstraintViolationException(violations)
+        }
+    }
+}
