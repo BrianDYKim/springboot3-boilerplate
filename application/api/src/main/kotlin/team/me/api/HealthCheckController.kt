ef1e0d17a40aebffa090b783de8e964e34f31fdb
DochiTutoring <dochi@tutoring.co.kr>
2024-01-28 23:05:08 +0900

    테스트

---
+++ application/api/src/main/kotlin/team/me/api/HealthCheckController.kt
@@ -0,0 +1,8 @@
+package team.me.api
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/")
+class HealthCheckController
