401cecc89fec98d290b978a5c037fb6af9f94c41
BrianDYKim <18k7102dy@gmail.com>
2024-09-08 16:33:36 +0900

    fix: Kotest 의존성 제거

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,6 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.springframework.boot.gradle.tasks.bundling.BootJar
 
 val mockkVersion = "1.10.3"
-val kotestVersion = "5.3.2"
 
 plugins {
     id("org.springframework.boot")
@@ -93,9 +92,6 @@ configure(subprojects.filter { it.name !in nonDependencyProjects }) {
 
         // Test
         testImplementation("io.mockk:mockk:$mockkVersion")
-        testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion") // for kotest framework
-        testImplementation("io.kotest:kotest-assertions-core:$kotestVersion") // for kotest core jvm assertions
-        testImplementation("io.kotest:kotest-property:$kotestVersion") // for kotest property test
 
         kapt("org.springframework.boot:spring-boot-configuration-processor")
     }
